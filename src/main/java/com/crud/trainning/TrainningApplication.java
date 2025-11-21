d2af574a4889e5bcee1cd77d7cdc180895d4c16e
Ricardo P. Reis <ricardoreishg@gmail.com>
2025-11-21 16:51:47 -0300

    Simple CRUD with feat: projeto inicial Spring Boot + MySQL + Lombok + Validation + DevTools

---
+++ src/main/java/com/crud/trainning/TrainningApplication.java
@@ -0,0 +1,13 @@
+package com.crud.trainning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningApplication.class, args);
+	}
+
+}
